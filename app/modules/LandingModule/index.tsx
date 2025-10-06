da962b1c040328c682c90c48ded8225531db24d5
Mednoob <ahmadyaqdhan123@gmail.com>
2025-10-06 11:52:11 +0900

    feat: add Icons

--- app/modules/LandingModule/index.tsx
+++ app/modules/LandingModule/index.tsx
@@ -1,10 +1,15 @@
 import React, { useEffect } from "react";
 import { Form, redirect, useLoaderData, useNavigate } from "react-router";
 
+import { Button } from "~/components/ui/button";
+
 export const LandingModule = () => {
   return (
     <main className="min-h-screen flex flex-col justify-center items-center">
-      Landing Page
+      <div className="flex flex-col">
+        <p>Landing Page</p>
+        <Button variant="transparentWhite" size="default">Test</Button>
+      </div>
       {/* {user ? (
         <Form method="post">
           <button type="submit">Sign Out</button>
