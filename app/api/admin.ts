9d6191b32c4160a0bd7073243bc9f04671f91101
VazhaK14 <khayrivazhasocial@gmail.com>
2025-11-11 08:53:04 +0700

    feat: add soal

--- app/api/admin.ts
+++ app/api/admin.ts
@@ -1,5 +1,30 @@
 import type { Tryout } from "~/modules/AdminModule/type";
+
 type TryoutUpdate = Partial<Tryout>;
+type TryoutCreate = {
+  name: string;
+  publishedAt: string;
+  closedAt: string;
+};
+
+type OpsiCreate = {
+  teks: string;
+  isCorrect: boolean;
+};
+
+type PembahasanCreate = {
+  pembahasan: string;
+};
+
+type SoalCreate = {
+  tryoutId: string;
+  subtestType: string;
+  tipeSoal: string;
+  question: string;
+
+  opsi?: OpsiCreate[];
+  pembahasanSoal?: PembahasanCreate;
+};
 
 export const getTryout = async () => {
   const res = await fetch(`${process.env.BACKEND_URL}/admin/tryout`);
@@ -25,6 +50,37 @@ export const updateTryout = async (id: string, data: TryoutUpdate) => {
 
   const json = await res.json();
 
+  if (!res.ok) {
+    if (json?.message) json.message;
+  }
+
+  return json;
+};
+
+export const createTryout = async (data: TryoutCreate) => {
+  const res = await fetch(`${process.env.BACKEND_URL}/admin/tryout`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  });
+
+  const json = await res.json();
+
+  if (!res.ok) {
+    if (json?.message) json.message;
+  }
+
+  return json;
+};
+
+export const createSoal = async (data: SoalCreate) => {
+  const res = await fetch(`${process.env.BACKEND_URL}/admin/soal`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  });
+
+  const json = await res.json();
   if (!res.ok) {
     if (json?.message) json.message;
   }
