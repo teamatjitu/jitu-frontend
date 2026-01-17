0ce9a43cf048d70583d066c8e7a5bfebf50e2480
aldofahrezy <aldofhrzy@gmail.com>
2026-01-17 11:14:54 +0700

    Reset password

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -4,5 +4,12 @@ export const authClient = createAuthClient({
   baseURL: process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:3000",
 });
 
-// Hapus 'setPassword' dari sini
-export const { signIn, signUp, useSession, signOut, changePassword } = authClient;
+export const { 
+  signIn, 
+  signUp, 
+  useSession, 
+  signOut, 
+  changePassword,
+  requestPasswordReset,
+  resetPassword
+} = authClient;
